3b2eadbf05f701b5699195d6861d910ab8a62af7
Wealthineer <hi@wealthineer.com>
2023-09-15 19:44:41 +0200

    added deserialization

--- models/Movie.ts
+++ models/Movie.ts
@@ -32,5 +32,27 @@ export class Movie {
         new Movie('The Godfather: Part II', 4, `The Godfather: Part II is a continuation of the saga of the late Italian-American crime boss, Francis Ford Coppola, and his son, Vito Corleone. The story follows the continuing saga of the Corleone family as they attempt to successfully start a new life for themselves after years of crime and corruption.`),
         new Movie('The Dark Knight', 5, `The Dark Knight is a 2008 superhero film directed, produced, and co-written by Christopher Nolan. Batman, in his darkest hour, faces his greatest challenge yet: he must become the symbol of the opposite of the Batmanian order, the League of Shadows.`),
     ]
+
+
+    static borshAccountSchema = borsh.struct([
+        borsh.bool('initialized'),
+        borsh.u8('rating'),
+        borsh.str('title'),
+        borsh.str('description'),
+      ])
+    
+      static deserialize(buffer?: Buffer): Movie|null {
+        if (!buffer) {
+            return null
+        }
+    
+        try {
+            const { title, rating, description } = this.borshAccountSchema.decode(buffer)
+            return new Movie(title, rating, description)
+        } catch(error) {
+            console.log('Deserialization error:', error)
+            return null
+        }
+      }
 }
 
